001901b4ad3a1091755bcd4599f046a9b86b8568
Islam Hanafi Mahmoud <40688222+islamhanafi94@users.noreply.github.com>
2021-05-30 11:23:30 +0200

    Update pet.routes.js

--- app/modules/pet/pet.routes.js
+++ app/modules/pet/pet.routes.js
@@ -3,12 +3,7 @@ const petController = require('./pet.controller');
 
 const petRouter = express.Router();
 
-/**
- * @swagger
- * /pets/:petId/bids:
- *   get:
- *     
- */
+
 petRouter.get('/:petId/bids', petController.getBidsOnSpecificPet);
 petRouter.post('/:petId/bids', petController.addBid);
 
